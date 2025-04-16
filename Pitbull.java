aca07679e1f264e56eadb8de8a3251c4fdd710c6
Paulo Henrique Epifanio de Oliveira <pfanio@gmail.com>
2025-04-16 19:00:20 -0300

    Aula 09/04/2025

---
+++ Pitbull.java
@@ -0,0 +1,11 @@
+public class Pitbull extends Cachorro{
+    
+    @Override
+    public void latir(){
+        System.out.println("ROuf Rouf");
+    }
+    
+    public void morder(){
+        System.out.println("Nhanc");
+    }
+}
---
+++ Shihtzu.java
@@ -0,0 +1,12 @@
+public class Shihtzu extends Cachorro {
+    
+    @Override
+    public void latir(){
+        System.out.println("Auu uu uu");
+    }
+    
+    @Override
+    public void chorar(){
+        System.out.println("cain!!!");
+    }
+}
